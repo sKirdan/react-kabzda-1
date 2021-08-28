efefe421300cd11868b5985d9199880e37a7813c
sKirdan <53087437+sKirdan@users.noreply.github.com>
2021-08-28 10:20:12 +0200

    fix bug

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,17 +5,20 @@ import userPhoto from '../../assets/images/171-1717870_stockvader-predicted-cron
 
 class Users extends React.Component {
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
 
         });
     }
 
     onPageChanged = (pageNumber) =>{
         this.props.setCurrentPage(pageNumber)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-            this.props.setTotalUsersCount(response.data.totalCount)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+
         })
     }
 
@@ -31,7 +34,7 @@ class Users extends React.Component {
             <div>
                 {pages.map(p => {
                     return <span className={this.props.currentPage === p && styles.selectedPage}
-                    onClick={ (e) => {this.onPageChanged(p); }}>{p}</span> })}
+                    onClick={ () => {this.onPageChanged(p); }}>{p}</span> })}
             </div>
             {
                 this.props.users.map(u => <div key={u.id}>
