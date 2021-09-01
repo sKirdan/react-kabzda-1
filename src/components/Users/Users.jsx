79a2a6a43ff2d471ef35fa5ea5fbb98c8873ddd6
sKirdan <53087437+sKirdan@users.noreply.github.com>
2021-09-01 08:34:07 +0200

    someFix

---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import styles from "./users.module.css";
+import userPhoto from "../../assets/images/171-1717870_stockvader-predicted-cron-for-may-user-profile-icon-png.png";
+
+let Users = props => {
+	let pagesCount = Math.ceil(props.totalUserCount / props.pageSize);
+
+	let pages = [];
+	for (let i = 1; i < pagesCount; i++) {
+		pages.push(i);
+	}
+
+	return (
+		<div>
+			<div>
+				{pages.map(p => {
+					return (
+						<span
+							className={props.currentPage === p && styles.selectedPage}
+							onClick={() => {
+								props.onPageChanged(p);
+							}}
+						>
+							{p}
+						</span>
+					);
+				})}
+			</div>
+			{props.users.map(u => (
+				<div key={u.id}>
+					<span>
+						<div>
+							<img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
+						</div>
+						<div>
+							{u.followed ? (
+								<button
+									onClick={() => {
+										props.unfollow(u.id);
+									}}
+								>
+									Unfollow
+								</button>
+							) : (
+								<button
+									onClick={() => {
+										props.follow(u.id);
+									}}
+								>
+									follow
+								</button>
+							)}
+						</div>
+					</span>
+					<span>
+						<span>
+							<div>{u.name}</div>
+							<div>{u.status}</div>
+						</span>
+						<span>
+							<div>{"u.location.country"}</div>
+							<div>{"u.location.city"}</div>
+						</span>
+					</span>
+				</div>
+			))}
+		</div>
+	);
+};
+export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,41 +1,66 @@
 import React from "react";
 import { connect } from "react-redux";
-import UsersAPIComponents from "./UsersAPIComponents";
-import {
-    followAC,
-    setCurrentPageAC,
-    setUsersTotalCountAC,
-    setUsersAC,
-    unfollowAC,
-} from "../../redux/users-reducer";
+import { followAC, setCurrentPageAC, setUsersTotalCountAC, setUsersAC, unfollowAC } from "../../redux/users-reducer";
+import Users from "./Users";
+import axios from "axios";
 
-let mapStateToProps = (state) => {
+class UsersContainer extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+
+    onPageChanged = pageNumber => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    };
+
+    render() {
+        return (
+            <Users
+                totalUserCount={this.props.totalUserCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                onPageChanged={this.onPageChanged}
+                users={this.props.users}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+            />
+        );
+    }
+}
+
+let mapStateToProps = state => {
     return {
         users: state.usersPage.users,
         pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.totalUserCount,
-        currentPage: state.usersPage.currentPage,
+        currentPage: state.usersPage.currentPage
     };
 };
-let mapDispatchToProps = (dispatch) => {
+let mapDispatchToProps = dispatch => {
     return {
-        follow: (usersId) => {
+        follow: usersId => {
             dispatch(followAC(usersId));
         },
-        unfollow: (usersId) => {
+        unfollow: usersId => {
             dispatch(unfollowAC(usersId));
         },
 
-        setUsers: (users) => {
+        setUsers: users => {
             dispatch(setUsersAC(users));
         },
-        setCurrentPage: (pageNumber) => {
+        setCurrentPage: pageNumber => {
             dispatch(setCurrentPageAC(pageNumber));
         },
-        setTotalUsersCount: (totalCount) => {
+        setTotalUsersCount: totalCount => {
             dispatch(setUsersTotalCountAC(totalCount));
-        },
+        }
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponents);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
