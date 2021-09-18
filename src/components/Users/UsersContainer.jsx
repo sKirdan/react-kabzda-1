4ba5992e86c59d758ed81c6a4004be6fcf4c2114
sKirdan <53087437+sKirdan@users.noreply.github.com>
2021-09-18 22:08:27 +0200

    DAL, axios.create

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -2,33 +2,28 @@ import React from "react";
 import { connect } from "react-redux";
 import { follow, setCurrentPage, setTotalUsersCount, setUsers, unfollow, toggleIsFetching } from "../../redux/users-reducer";
 import Users from "./Users";
-import axios from "axios";
+import * as axios from 'axios'
 import Preloader from "../common/Preloader/Preloader";
+import { getUsers } from "../../api/api";
 
 class UsersContainer extends React.Component {
     componentDidMount() {
         this.props.toggleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
-            {
-                withCredentials: true
-            })
-            .then(response => {
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount);
-                this.props.toggleIsFetching(false)
+        getUsers(this.props.currentPage, this.props.pageSize).then(data => {
+            debugger
+            this.props.setUsers(data.items);
+            this.props.setTotalUsersCount(data.totalCount);
+            this.props.toggleIsFetching(false)
 
-            })
+        })
     }
 
     onPageChanged = pageNumber => {
         this.props.setCurrentPage(pageNumber);
         this.props.toggleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
-            {
-                withCredentials: true
-            })
-            .then(response => {
-                this.props.setUsers(response.data.items);
+        getUsers(pageNumber, this.props.pageSize)
+            .then(data => {
+                this.props.setUsers(data.items);
                 this.props.toggleIsFetching(false)
             });
     };
